665bd22742ae97b183aea11d898cfefae1279ce7
hanse174 <hanse174@mail.nmc.edu>
2018-11-01 21:44:06 -0400

    Absolutely no idea what I'm doing...

---
+++ Wpf_SimpleCalculator/test/MainWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wpf_SimpleCalculator.test;
+
+namespace Wpf_SimpleCalculator
+{
+    class MainWindowViewModel:BindableBase
+    {
+        public MainWindowViewModel()
+        {
+            NavCommand = new MyICommand<string>(OnNav);
+        }
+
+        private PortalViewModel portalViewModel = new PortalViewModel();
+
+        private BindableBase _CurrentViewModel;
+
+        public BindableBase CurrentViewModel
+        {
+            get { return _CurrentViewModel; }
+            set { SetProperty(ref _CurrentViewModel, value); }
+        }
+
+        public MyICommand<string> NavCommand { get; private set; }
+
+        private void OnNav(string destination)
+        {
+
+            switch (destination)
+            {
+                case "portal":
+                    CurrentViewModel = portalViewModel;
+                    break;
+                case "customers":
+                default:
+                    CurrentViewModel = portalViewModel;
+                    break;
+            }
+        }
+    }
+}
---
+++ Wpf_SimpleCalculator/test/PortalViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_SimpleCalculator.test
+{
+    public class PortalViewModel:BindableBase
+    {
+        
+    }
+}
